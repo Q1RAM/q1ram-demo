97a8e970c7f4a94f6d5e827812d3313c7e820c78
ahmed elbasheq <ahmed.m.easa@gmail.com>
2025-06-05 22:32:34 +0300

    refactor: adjust logo placement and width for improved layout in Q1RAM demo

--- app.py
+++ app.py
@@ -176,7 +176,9 @@ def start_over():
 st.set_page_config(page_title="Q1RAM Demo", page_icon=":guardsman:")
 
 # Insert a static image and title in the same row at the top
-st.image("q1ram_logo.jpg",width=100)
+header_col1, header_col2, header_col3 = st.columns([1, 2, 1])  # Adjust column widths as needed
+with header_col1:  # Place the title in the left column
+    st.image("q1ram_logo.jpg",width=200)
 
 # st.image("qram.png", width=300)  # Adjust width as needed
 # with header_col2:
